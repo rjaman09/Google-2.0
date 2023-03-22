064598627a142a74d358153a825b4a0b48dd2d46
rjaman09 <kumawataman6378@gmail.com>
2023-03-22 11:23:23 +0530

    install next js

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Google</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Google clone created by Nextjs" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+};
