e6e1baa2d1ea5098d26573e0ad1fe2113a21e5f4
rjaman09 <kumawataman6378@gmail.com>
2023-03-22 18:44:29 +0530

    add body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
 
 export default function Home() {
   return (
     <>
       <HomeHeader />
+
+      <div className="flex flex-col items-center mt-24">
+        <Image width="300" height="100" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png" alt="Google Image" />
+
+        <HomeSearch />
+      </div>
     </>
   );
 };
